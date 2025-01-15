f8c6f45543c605e45a1b4c8fb9fa32bd89fc0b9e
jloliveira95 <jl.oliveira95@gmail.com>
2025-01-15 11:37:49 -0300

    Criando Repository

---
+++ backend_01/src/main/java/com/generation/backand_01/repository/ColaboradoresRepository.java
@@ -0,0 +1,9 @@
+package com.generation.backand_01.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.generation.backend_01.model.Colaboradores;
+
+public interface ColaboradoresRepository extends JpaRepository<Colaboradores,Long> {
+	
+}
